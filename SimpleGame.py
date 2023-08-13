06b7d70964efef1c994d2f3bb0b612cf150286c1
todd.cochrane <todd.cochrane@carboncrop.nz>
2023-08-13 13:22:12 +1200

    Show Map - with RANDOM layout :-)

--- SimpleGame.py
+++ SimpleGame.py
@@ -2,46 +2,87 @@
 A comment describing the game module
 """
 import PySimpleGUI as sg
-
+import random
 
 import cmd_parser.command_manager as cm
 
 
 def modal_map_window():
     # Extra fun :-) !
-    can = sg.Canvas(size=(700, 500), background_color='grey', key='canvas')
+    place_width = 100
+    off_set = place_width/2
+
+    def to_pos(pLoc):
+        return 10 + (pLoc*place_width)
+
+    map_structure = {key: [
+        cm.game_places[key][next_place][1] for next_place in cm.game_places[key]
+        if next_place != "Story" and next_place != "Image"] for key in cm.game_places}
+
+    total_places = len(map_structure)
+    print(total_places, map_structure)
+    # draw the map with two way paths
+    # create a set of edges.
+    edges = ()
+    for key in map_structure:
+        for place in map_structure[key]:
+            edge = tuple(frozenset((key, place)))
+            if edge not in edges:
+                edges += (edge,)
+
+    print(edges)
+
+    locations = {key: (random.randint(0, total_places - 1),
+                       random.randint(0, total_places - 1),)
+                 for key in map_structure
+                 }
+
+    print(locations)
+
+    # Set up a window with a canvas on it
+
+    can = sg.Canvas(size=((place_width + 10)*total_places, (place_width + 10)
+                    * total_places), background_color='grey', key='canvas')
     layout = [[can]]
     window = sg.Window('Canvas Example - Modal Map', layout, finalize=True)
 
     tkc = can.TKCanvas
-    row_height = 20
-    row = 1
-    col = 1
+
     fig = []
 
-    for key in cm.game_places:
-        fig += [tkc.create_text(col*40, row*row_height, text=key)]
-        col += 1
-        for item in cm.game_places[key]:
-            if item not in "StoryImage":
-                row += 1
-                fig += [tkc.create_text(col*40, row*row_height, text=item)]
+    # Draw Edges
+    for edge in edges:
+        from_row, from_col = locations[edge[0]]
+        from_x = to_pos(from_col)
+        from_y = to_pos(from_row)
 
-                next_place = cm.game_places[key][item]
-                row += 1
-                col += 1
-                fig += [tkc.create_text(col*40,
-                                        row*row_height, text=next_place[1])]
-                col -= 1
+        to_row, to_col = locations[edge[1]]
+        to_x = to_pos(to_col)
+        to_y = to_pos(to_row)
 
-        col = 1
-        row += 1
+        fig += [tkc.create_line(from_x+off_set-10, from_y+off_set-10, to_x+off_set-10,
+                                to_y+off_set-10, fill='green', width=5)]
+    # Draw Places
+    prev_location = tuple()
+
+    for key in locations:
+        location = locations[key]
+        row, col = location
+        x = to_pos(col)
+        y = to_pos(row)
+        colour = "blue"
+        show_text = key
+        if cm.game_state == key:
+            colour = "yellow"
+            show_text = "YOU ARE HERE \n"+key
+        fig += [tkc.create_oval(x, y, x+off_set, y+off_set, fill=colour),
+                tkc.create_text(x, y, text=show_text)
+                ]
 
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
-    pass
 
 
 def make_a_window():
